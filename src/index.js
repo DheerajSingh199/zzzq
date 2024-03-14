62d1924d38fe599e108377e9497d8c77532d5979
DheerajSingh199 <dheerajsingh16496@gmail.com>
2024-03-15 01:02:00 +0530

    APi Error utils code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import dotenv from "dotenv";
 import connectDb from "./db/db.js";
 
-dotenv.config({path:'./env' });
-connectDb();
+dotenv.config({ path: "./env" });
 
+
+
+connectDb()
+.then(() => {
+  app.on("Error", (Error) => {
+    console.log(`Error: ${Error} `);
+    throw Error;
+  });
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`serve are running in port ${process.env.PORT || 8000}`);
+  });
+})
+.catch((err) => {
+  console.log(`mongo db connection faiiiil ${err}`);
+});
